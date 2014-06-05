34631d76db97ed480baa567e1a2dfdb8b81c67bd
Jack <xhzengAIB@gmail.com>
2014-06-05 16:07:37 +0800

    修改注释

--- XHNewsParsingSwift/XHNewsParsingSwift/AppDelegate.swift
+++ XHNewsParsingSwift/XHNewsParsingSwift/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  XHNewsParsingSwift
 //
 //  Created by dw_iOS on 14-6-5.
-//  Copyright (c) 2014年 广州华多网络科技有限公司 多玩事业部 iOS软件工程师 曾宪华. All rights reserved.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
 //
 
 import UIKit
